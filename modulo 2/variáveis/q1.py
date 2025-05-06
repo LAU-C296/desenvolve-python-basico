d63b8847e05df81a8635f291d1f1c863dab47648
Laura Araújo de Queiroz <araujodequeirozlaura@gmail.com>
2025-05-06 16:09:36 -0300

    Add files via upload

---
+++ modulo 2/variáveis/q1.py
@@ -0,0 +1,14 @@
+# Preencha o print com as alternativas corretas
+# separadas por espaços em branco.
+# Ex: print("a b c")
+print("nome_completo")
+
+print("idade")
+
+print("MediaNotas")
+
+print("_tipo")
+
+print("2total")
+
+print("lambda")
---
+++ modulo 2/variáveis/q2.py
@@ -0,0 +1,8 @@
+# Q2 - script 1 ao 4 
+print("7")
+
+print("Não foi dado um valor para n1 ou n2. Não é possível fazer a soma, porque não tem as aspas.") 
+
+print("Não foi dado um novo valor a variável a, portanto não é possível caular o novo valor de a.")
+
+print("a palavra juros deveria  estar antes do 0.1")
